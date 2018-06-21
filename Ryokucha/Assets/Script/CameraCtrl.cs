221ce6b0124651b921d6872524dc467b42f47e3f
denchu <kuroko4491@yahoo.co.jp>
2018-06-21 23:45:31 +0900

    横スクロールにシフト
    背景と地面をスクロールさせた

---
+++ Ryokucha/Assets/Script/CameraCtrl.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraCtrl : MonoBehaviour {
+
+    private Transform player;
+
+    // Use this for initialization
+    void Start () {
+        player = GameObject.Find("Player").transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(player.position.x, transform.position.y, transform.position.z);
+	}
+}
---
+++ Ryokucha/Assets/Script/ScrollGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollGround : MonoBehaviour {
+
+    public int spriteCount = 3;
+    public float width;
+    public float movepoint;
+    private Transform player;
+
+    private void Start()
+    {
+        player = GameObject.Find("Player").transform;
+    }
+
+    void Update()
+    {
+        if(player.position.x - transform.position.x > movepoint && transform.position.x < player.position.x) {
+            Move();
+        }
+    }
+
+    void Move()
+    {
+        // 幅ｘ個数分だけ右へ移動
+        transform.position += Vector3.right * width * spriteCount;
+    }
+}
